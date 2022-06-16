b9ff2805919349aaaf337e7c4edb4e47f09c4a2a
Maheshkumar N <pn.maheshkumar@gmail.com>
2022-06-16 11:09:56 +0530

    PSExecution

--- 03.01InstallGitPosh.ps1
+++ 03.01InstallGitPosh.ps1
@@ -1,5 +1,7 @@
-# Setting Powershell for Git
+# Run this command on both Powershell for installing the scripts and module
+Set-ExecutionPolicy Bypass -scope CurrentUser -Force
 
+# Setting Powershell for Git
 Install-Module -Name Terminal-Icons -Repository PSGallery -Force
 Install-Module -Name PSReadLine -AllowPrerelease -Scope CurrentUser -Force -SkipPublisherCheck
 
